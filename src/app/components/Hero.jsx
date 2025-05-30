e5824a6500761b8aa91618f8b03e7b638a0c2cb7
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-05-30 21:30:34 +0530

    addedButton

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     const handleSchedule = () => {
         window.open("https://calendly.com/salonitcn/30min?month=2025-05", "_blank")
     }
-    
+
     return (
         <div className='px-2 xl:px-0 w-full mx-auto bg-black flex flex-col items-center pt-40 pb-10 space-y-5' style={{ background: `url(${mainFrame.src})`, backgroundPosition: "center", backgroundSize: 'cover' }}>
             <div className='bg-[#272423] px-3 py-1 body-text text-sm rounded-xl w-fit'>
@@ -20,8 +20,8 @@ const Hero = () => {
                 </div>
                 <div className='body-text w-[90%] xl:w-[70%] mx-auto space-y-5'>
                     <p className='text-base md:text-xl xl:text-2xl font-light secondary-text-color'>From sourcing the right product to shaping the perfect branding, we craft the journey from shelf to spotlight.</p>
-                    <button onClick={handleSchedule} className="orange-bg py-3 px-7 w-full xl:w-fit text-sm xl:text-base rounded-3xl">Schedule A Call </button>
-                </div>
+                    <button onClick={handleSchedule} className="hover:cursor-pointer orange-bg py-3 px-7 w-full xl:w-fit text-sm xl:text-base rounded-3xl">Schedule A Call </button>
+                </div>  
             </div>
             <video
                 src="/assets/mainVideo.mp4"
