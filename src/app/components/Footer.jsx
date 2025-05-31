0513130b92d31815b6d9331fb1fe44804c9dc4a7
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-05-31 18:28:17 +0530

    changedCss

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -65,8 +65,6 @@ const Footer = () => {
             setLoading(false);
             setFormData({}); setSelectedOption(''); setOtherInput('');
         }
-
-
     }
 
     const handleSchedule = () => {
@@ -77,17 +75,17 @@ const Footer = () => {
         <>
             <div className='w-full px-2 xl:px-0 xl:w-[70%] mx-auto flex flex-col md:flex-row py-10 md:py-20 border-b-2 border-b-gray-800 gap-10'>
                 <div className='w-full xl:w-1/2 px-5 space-y-7 body-text'>
-                    <Image src={footerLogo.src} alt='brandLogo.png' height={200} width={300} className='h-[150px] w-[250px] mx-auto' />
+                    {pathname !== "/contact-us" && <Image src={footerLogo.src} alt='brandLogo.png' height={200} width={300} className='h-[150px] w-[250px] mx-auto' />}
                     <p className='text-sm xl:text-xl font-light'>Want to see if we’re the right fit to work together? Schedule a quick 15 min zoom chat with us to clear up any questions you may have.</p>
-                    <div className='flex justify-center'>
+                    <div className={` ${pathname !== "/contact-us" ? "flex justify-start w-full border" : "pb-5"} `}>
                         <button onClick={handleSchedule} className="hover:cursor-pointer orange-bg px-4 py-2 w-[70%] mx-auto xl:py-3 xl:px-7 xl:w-fit text-sm xl:text-base rounded-3xl">Schedule A Call </button>
                     </div>
                     <div>
-                        <h1 className='text-xl font-light'>New Business Enquiries:</h1>
-                        <p className='orange-text text-base xl:text1-xl font-light'>contact@thebrandandport.com</p>
+                        <h1 className='xl:text-2xl font-light'>New Business Enquiries:</h1>
+                        <p className='orange-text text-base xl:text-xl font-medium'>contact@thebrandandport.com</p>
                     </div>
                 </div>
-                <div className={`w-full xl:w-1/2 px-5 xl:px-0 body-text space-y-7 md:mt-12 ${pathname === "/contact-us" && "md:mt-12"}`}>
+                <div className={`w-full xl:w-1/2 px-5 xl:px-0 body-text space-y-7 ${pathname !== "/contact-us" && "md:mt-12"}`}>
                     {pathname !== "/contact-us" && <h1>Let us create an extraordinary brand that elevates your business. Please fill out the following form, and one of us will get back to you as soon as possible.</h1>}
                     <div className='grid grid-cols-1 xl:grid-cols-2 gap-y-5'>
                         <div className='space-y-2'>
--- src/app/private-label/page.jsx
+++ src/app/private-label/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
             <div className='w-full px-2 xl:px-0 mx-auto bg-black flex flex-col items-center py-30 pb-20space-y-5' style={{ background: `url(${mainFrame.src})`, backgroundPosition: "center", backgroundSize: 'cover' }}>
                 <div className='flex flex-col space-y-3 lg:space-y-0 text-center lg:flex-row lg:justify-between w-[80%] xl:w-[70%] xl:gap-10 mx-auto mt-20'>
                     <h1 className='heading-text text-3xl md:text-5xl xl:text-7xl'>Private label</h1>
-                    <p className='body-text secondary-text-color text-base md:text-xl lg:w-[50%]'>We are passionate about exploring possibilities of classically modern-day lifestyles. And we leave no gap while working towards making that available to people via our range of products.</p>
+                    <p className='body-text text-left secondary-text-color text-base md:text-xl lg:w-[50%]'>We are passionate about exploring possibilities of classically modern-day lifestyles. And we leave no gap while working towards making that available to people via our range of products.</p>
                 </div>
             </div>
 
