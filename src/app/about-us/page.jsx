3b389e3920e3fddf32933d5ad99fe91163e303a2
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-05-31 18:18:59 +0530

    padding

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -7,7 +7,7 @@ import { TARGET_AUDIENCE } from '@/data'
 const page = () => {
     return (
         <>
-            <div className='w-full px-2 xl:px-0 mx-auto bg-black flex flex-col items-center py-20 pb-10 space-y-5' style={{ background: `url(${mainFrame.src})`, backgroundPosition: "center", backgroundSize: 'cover' }}>
+            <div className='w-full px-2 xl:px-0 mx-auto bg-black flex flex-col items-center py-30 pb-20 space-y-5' style={{ background: `url(${mainFrame.src})`, backgroundPosition: "center", backgroundSize: 'cover' }}>
                 <div className='flex flex-col space-y-3 lg:space-y-0 text-center lg:flex-row lg:justify-between w-[80%] xl:w-[60%] mx-auto mt-20'>
                     <h1 className='heading-text text-3xl md:text-5xl xl:text-7xl'>About Us</h1>
                     <p className='body-text secondary-text-color text-base md:text-xl lg:w-[60%]'>We build brands that travel well across borders and browsers.
