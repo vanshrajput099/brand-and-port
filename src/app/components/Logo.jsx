f4d3bf9d7501bb5e08787e24ddb449f36b63fe41
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-06-02 16:04:37 +0530

    fixedLogo

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -7,9 +7,17 @@ const Logo = () => {
         window.open("https://calendly.com/salonitcn/30min?month=2025-05", "_blank")
     }
     return (
-        <div onClick={handleSchedule} className="hover:cursor-pointer right-5 bottom-5 xl:right-10 xl:bottom-10 fixed">
-            <img className="h-20 w-20 xl:h-30 xl:w-30 animate-up-down" src={orange.src} alt="" />
+        <div
+            onClick={handleSchedule}
+            className="hover:cursor-pointer right-5 bottom-5 xl:right-10 xl:bottom-10 fixed z-[9999]"
+        >
+            <img
+                className="h-20 w-20 xl:h-30 xl:w-30 animate-up-down"
+                src={orange.src}
+                alt="logo"
+            />
         </div>
+
     )
 }
 
